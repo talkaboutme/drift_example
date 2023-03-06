cbd3c775c3598d6c8c719b2db37e1456abf2171c
talkaboutme <34934369+talkaboutme@users.noreply.github.com>
2023-03-06 12:37:12 +0900

    웹용 vscode에서 커밋 및 푸시

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Drift',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
